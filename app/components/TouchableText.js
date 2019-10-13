38c28c6d1d11f4a0b24aae6116336feb2da1038d
Priscilla <pririvera19@gmail.com>
2019-10-12 20:13:27 -0600

    termine de maquetar
    agregue la imagen y la card

---
+++ app/components/TouchableText.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import MainStyles from '../styles/MainStyles';
+
+export default function TouchableText(props) {
+    return(
+        <View style={props.alignCenter? MainStyles.TouchableTextContainerRow : MainStyles.TouchableTextContainer}>
+            <Text>{props.outerText}</Text>
+            <TouchableOpacity
+                onPress={props.opPress}>
+                <Text
+                    style={MainStyles.TouchableText}>
+                    {props.innerText}
+                </Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
